9700d7fec90e499d366ed61b5244cdfacf4bb530
mdh00 <madhini00@gmail.com>
2026-03-04 00:07:19 +0530

    feat: models created

---
+++ BackendServices/cv-service/src/main/java/com/hireflow/cvservice/model/enums/ApplicationStatus.java
@@ -0,0 +1,13 @@
+package com.hireflow.cvservice.model.enums;
+
+public enum ApplicationStatus {
+
+    APPLIED,
+    UNDER_REVIEW,
+    SHORTLISTED,
+    FORWARDED_TO_INTERVIEW,
+    INTERVIEW_SCHEDULED,
+    REJECTED,
+    HIRED,
+    WITHDRAWN
+}
